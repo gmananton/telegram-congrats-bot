777569de5c72da208afe9b7a1a9d06d0a1d9f99a
Михайлов Антон Владимирович <AVMikhaylov@alfabank.ru>
2018-02-09 15:58:24 +0300

    sql added

--- src/main/java/com/gman/telegram/Application.java
+++ src/main/java/com/gman/telegram/Application.java
@@ -1,10 +1,12 @@
-package ru.alfabank.instr;
+package com.gman.telegram;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/gman/telegram/controller/IndexController.java
+++ src/main/java/com/gman/telegram/controller/IndexController.java
@@ -1,13 +1,19 @@
-package ru.alfabank.instr.controller;
+package com.gman.telegram.controller;
 
+import com.gman.telegram.bot.Bot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
+
+    @Autowired
+    Bot bot;
+
     @RequestMapping("index")
     public String index() {
-        return "index";
+        return bot.getBotUsername() + " : " + bot.getBotToken();
     }
 }
